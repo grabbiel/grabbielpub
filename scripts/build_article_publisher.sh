79f6610c6fb4671e64fd34ce212d6547b86fa574
grabbiel <fcruzado22@gmail.com>
2025-04-01 14:36:05 -0700

    fixing build?

--- scripts/build_article_publisher.sh
+++ scripts/build_article_publisher.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-SRC_PATH="/tmp/article_publisher.cpp"
+SRC_DIR="/tmp/article-publisher"
 OUT_PATH="/usr/local/bin/article_publisher"
 
 echo "[*] Building article_publisher..."
-g++ -std=c++17 -O2 -o "$OUT_PATH" "$SRC_PATH" -lsqlite3
+g++ -std=c++17 -O2 -o "$OUT_PATH" \
+  "$SRC_DIR/article_publisher.cpp" \
+  "$SRC_DIR/https_server.cpp" \
+  -lsqlite3
 chmod +x "$OUT_PATH"
 echo "[+] Build complete: $OUT_PATH"
