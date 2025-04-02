4363541d3185399c9be3d9d82db818a5b6e8e059
grabbiel <fcruzado22@gmail.com>
2025-04-01 18:42:34 -0700

    permissions fix

--- scripts/build_article_publisher.sh
+++ scripts/build_article_publisher.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
 set -e
 
 SRC_DIR="/tmp/article-publisher"
+BUILD_PATH="/tmp/article_publisher"
 OUT_PATH="/usr/local/bin/article_publisher"
 
-echo "[*] Building article_publisher..."
-g++ -std=c++17 -O2 -o "$OUT_PATH" \
+echo "[*] Compiling to $BUILD_PATH..."
+g++ -std=c++17 -O2 -o "$BUILD_PATH" \
   "$SRC_DIR/article_publisher.cpp" \
   "$SRC_DIR/https_server.cpp" \
   -lsqlite3
-chmod +x "$OUT_PATH"
-echo "[+] Build complete: $OUT_PATH"
+
+echo "[*] Moving binary to $OUT_PATH..."
+sudo mv "$BUILD_PATH" "$OUT_PATH"
+sudo chmod +x "$OUT_PATH"
+
+echo "[+] Build complete and deployed to $OUT_PATH"
